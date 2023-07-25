312039ca7734c617211681b94dc0e967456180b0
Kadir Eskiçırak <132097106+kadirbrkm@users.noreply.github.com>
2023-07-25 14:05:30 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,179 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+
+
+    int count = 0, ans1, ans2, ans3, ans4, ans5;
+
+    cout << "Question 1:\n";
+
+    cout << "Which one of the following cause global warming?\n";
+
+    cout << "1) Carbon dioxide\n2) Oxygen\n3) Nitrogen\n4) Hydrogen\n";
+
+
+
+    cout << "Enter the correct option: ";
+
+
+
+    cin >> ans1;
+
+
+
+    if (ans1 == 1)
+
+    {
+
+
+
+        count++;
+
+    }
+
+    cout << "Question 2:\n";
+
+    cout << "What is the main reason for melting of ice sheets?" << endl;
+
+    cout << "1) Increase in the oxygen content\n2) Global warming\n3) Decrease in carbon dioxide content\n4) Noise pollution" << endl;
+
+    cout << "Enter the correct option: ";
+
+
+
+    cin >> ans2;
+
+
+    if (ans2 == 2)
+
+    {
+
+
+
+        count++;
+
+    }
+
+    cout << "Question 3:\n";
+
+    cout << "Which of the following gas does not contribute to the global warming?" << endl;
+
+    cout << "1) Methane\n2) Carbon dioxide\n3) Sulphur\n4) Acetylene" << endl;
+
+
+
+    cout << "Enter the correct option: ";
+
+
+
+    cin >> ans3;
+
+
+    if (ans3 == 4)
+
+    {
+
+
+
+        count++;
+
+    }
+
+    cout << "Question 4:\n";
+
+    cout << "Human activities cause global warming. True or False?\n";
+
+    cout << "1) True\n2) False\n";
+
+
+
+    cout << "Enter the correct option: ";
+
+
+
+    cin >> ans4;
+
+
+
+    if (ans4 == 1)
+
+    {
+
+
+
+        count++;
+
+    }
+
+    cout << "Question 5:\n";
+
+    cout << "Which one of the following result takes place due to global warming?" << endl;
+
+    cout << "1) Maintaining steady temperature\n2) Changes in the rainfall\n3) Pleasant environment\n4) Causing less pollution\n";
+
+
+
+    cout << "Enter the correct option: ";
+
+
+
+    cin >> ans5;
+
+
+
+    if (ans5 == 2)
+
+    {
+
+
+
+        count++;
+
+    }
+
+
+    if (count == 5)
+
+    {
+
+        cout << "Excellent";
+
+    }
+
+
+    else if (count == 4)
+
+    {
+
+        cout << "Very good";
+
+    }
+
+
+
+    else if (count < 4)
+
+    {
+
+        cout << "Time to brush up on your knowledge on global warming\n";
+
+        cout << "There you go some websites:\n";
+
+        cout << "https://www.nrdc.org/stories/global-warming-101" << endl;
+
+
+        cout << "https://www.nationalgeographic.com/environment/global-warming/global-warming-overview/" << endl;
+
+
+    }
+
+    return 0;
+
+}
